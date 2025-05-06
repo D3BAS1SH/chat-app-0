c9df8824d25f4b5324c8d01a942549ce728f3417
D3BAS1SH <Morning.Star00.111@protonmail.com>
2025-05-07 01:57:27 +0530

    Auth setup completed.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
+import Button from "@/components/ui/Button";
+import { db } from "@/lib/db";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+
+  await db.set('hello',"hello")
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Hello World
-    </div>
+    <Button>
+      Hello
+    </Button>
   );
 }
